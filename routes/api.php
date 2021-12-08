86648656b350a9568a16604d45ae678131fd515b
yurdmitriev <yurdmitriev02@gmail.com>
2021-12-09 01:17:37 +0200

    Auth flow with Bearer token

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->get('/name', function (Request $request) {
+    return response()->json(['name' => $request->user()->name]);
+});
+
+Route::prefix('sanctum')->group(function() {
+    Route::post('register', 'App\Http\Controllers\API\AuthController@register');
+    Route::post('token', 'App\Http\Controllers\API\AuthController@token');
+});
+
