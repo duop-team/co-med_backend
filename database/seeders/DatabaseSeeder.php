f4fee98b3c3d28a7c0afe8d6055ddf47e4ea2afb
yurdmitriev <yurdmitriev02@gmail.com>
2021-12-09 03:45:50 +0200

    Initial db data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $roles = ["admin", "staff", "doctor", "user"];
+
+        foreach ($roles as $role) {
+            DB::table('roles')->insert([
+                'name' => $role
+            ]);
+        }
+
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@comed',
+            'password' => Hash::make('admin123'),
+            'phone' => '+380681234567',
+            'role_id' => 1
+        ]);
+
         // \App\Models\User::factory(10)->create();
     }
 }
