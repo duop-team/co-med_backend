3047aef437d2c1eade36e015701e73cb96d72bc8
yurdmitriev <yurdmitriev02@gmail.com>
2021-12-09 03:45:31 +0200

    Roles initial commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,10 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->integer('age')->nullable();
+            $table->tinyInteger('gender')->nullable();
+            $table->foreignId('role_id')->constrained('roles');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
